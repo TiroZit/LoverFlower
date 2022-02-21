2f47e782122684f32f440892bbafa3567aeb3f2f
TiroZit <nikus.master@gmail.com>
2022-02-21 19:04:37 +0500

    fix

--- gulp/config/ftp.js
+++
@@ -1,7 +0,0 @@
-export let configFTP = {
-  host: '130.61.150.199',// Адрес ftp
-  port: '21',// Порт
-  user: 'root',// Пользователь
-  password: '',// Пароль
-  parallel: 3,// Кол-во одновременных потоков
-}
